698d35ec39a09c8d0577a1cf2f9f5f8c21ee46c0
ShivaniGondha <97283825+ShivaniGondha@users.noreply.github.com>
2024-01-26 19:56:12 +0530

    starting of React Udemy course

---
+++ React_Udemy/1 ultimate-react-course-main/02-JS-review/My Code/script.js
@@ -0,0 +1,204 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      //array
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      //object
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+//Destructing
+
+//1
+const books = getBooks();
+books;
+
+//2
+const bookbyid = getBook(1);
+
+//this
+/* const title = bookbyid.title;
+title;
+const author = bookbyid.author;
+author;
+console.log(title, " - ", author); */
+
+//or this
+const { title, author, pages, genres } = bookbyid; //objectArray
+console.log(author, "-", title, "-", pages, "-", genres);
+
+//3
+//split object   this
+/* const primaryGenre = genres[1];
+const secondaryGenre = genres[2];
+console.log(primaryGenre, "-", secondaryGenre); */
+
+//or this
+/* const [primaryGenre, secondaryGenre] = genres;
+console.log(primaryGenre, "-", secondaryGenre); */
+
+//for all genres - rest operator
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, "-", secondaryGenre, otherGenres);
+
+//spread operator - just like distructing we can use it in both array and objects.
+//we can use it basically to add properties. or by overiding them.
+const newGenres = [...genres, "epic"];
+newGenres;
+
+const updatedbook = {
+  ...books,
+  moviePublicationDate: "2001-12-19", //adding a new property
+  pages: 234, //overwriting an existing property
+};
+updatedbook;
+
+// template literals .....................
+//are an ES6 JavaScript feature which allows us to very easily create strings that contain some JavaScript variables, or actually that contains any JavaScript expression, inside of a string.
+const summary = `a book`;
+summary;
+
+const summary1 = `${title} is a ${pages}-page long book written by ${author}.`;
+summary1;
+
+//ternary
+const pagesRange = pages > 1000 ? "over a thousant" : "less than thousand";
+pagesRange;
