34cf2cff5f5124d3859e696323e8ed50af7b5cea
ShivaniGondha <97283825+ShivaniGondha@users.noreply.github.com>
2024-02-07 02:05:36 +0530

    3 Pizza project - Separation of Concerns, Styling React Applications, Passing and Receiving Props

--- React_Udemy/My Projects/03-pizza-menu/src/index.js
+++ React_Udemy/My Projects/03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,44 +58,72 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  //const style = { color: "red", fontSize: "40px", textTransform: "uppercase" };
+  const style = {};
+  return (
+    /* 
+    <h1 style={{ color: "red", fontSize: "40px", textTransform: "uppercase" }}>
+      Fast React Pizza Co.
+    </h1>
+    */
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/Prosciutto.jpg"
+        price={300}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={100}
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  //console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>Price : {props.price + 99} Rs.</span>
+      </div>
     </div>
   );
 }
 
 function Footer() {
+  /* 
   const hour = new Date().getHours();
-  const openHour = 9;
-  const closeHour = 22;
+  const openHour = 9; //9am
+  const closeHour = 22; //10pm
 
-  if (hour >= openHour && hour < closeHour) alert("We're Currently open !!");
-  else alert("Sorry, We're Closed :((( ");
-
+  if (hour >= openHour && hour < closeHour) alert("We're Currently open :) ");
+  else alert("Sorry, We're Closed :( ");
+ */
   return (
-    <footer>{new Date().toLocaleTimeString()} We're currently Open !!</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We're currently Open !!
+    </footer>
   );
 
   //return React.createElement("footer", null, "We're currently Open !!");
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/Prosciutto.jpg" alt="Prosciutto pizza" />
-      <h2>Pizza Prosciutto</h2>
-      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-    </div>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
